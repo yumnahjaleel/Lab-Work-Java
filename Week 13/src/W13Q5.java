a996adff46fd28d53771a3d4f32102704acd862a
Yumnah Jaleel <yumnahjaleel05@gmail.com>
2025-11-16 17:21:14 +0530

    Update W13Q5.java

--- Week 13/src/W13Q5.java
+++ Week 13/src/W13Q5.java
@@ -1,10 +1,21 @@
+//Write a java program to find total no. of lines in a file.
+import java.io.IOException;
 import java.io.*;
-
-//Write a Java Program to create a new file.
-
 public class W13Q5 {
-    public static void main(String[] args) throws Exception{
-//        Object file;
-//        BufferedReader br = new BufferedReader(file.txt);
+    public static void main(String[] args) {
+        int lineCount=0;
+        try{
+            BufferedReader f = new BufferedReader(new FileReader("A.txt"));
+
+            String line;
+            while((line=f.readLine())!=null){
+                lineCount++;
+            }
+            f.close();
+            System.out.println("Total number of lines in the file: "+lineCount);
+        } catch (IOException e) {
+            System.out.println("Error!");
+        }
     }
 }
+
