1747c33bcdb4c4e0216871d37ac70d84d7773183
Yumnah Jaleel <yumnahjaleel05@gmail.com>
2025-11-16 17:19:28 +0530

    Modify W13Q2 to write user input to a file
    Updated the program to allow user input for file writing.

--- Week 13/src/W13Q2.java
+++ Week 13/src/W13Q2.java
@@ -1,12 +1,22 @@
-import java.io.*;
-
 //Write a Java Program to write into a file.
-
+import java.io.*;
+import java.util.Scanner;
+import java.io.FileWriter;
 public class W13Q2 {
-    public static void main(String[] args) throws Exception{
-        FileWriter fw = new FileWriter("file.txt");
-        fw.write("Hello world!");
-        fw.write(" This is java.");
-        fw.close();
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        FileWriter f =new FileWriter("A.txt"); ;
+        try {
+            f = new FileWriter("A.txt");
+            System.out.println("Enter the content to be written into the file: ");
+            String string = sc.nextLine();
+            f.write(string);
+            System.out.println("Successfully wrote data!");
+        } catch (IOException e) {
+            System.out.println("Unable to write into file!");
+        } finally {
+            f.close();
+            sc.close();
+        }
     }
 }
